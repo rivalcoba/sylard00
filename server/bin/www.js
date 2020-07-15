54b2fd81c1a233f7154a8c34821c8424fe6dd686
rivalcoba <ivan.rivalcoba@gmail.com>
2020-07-15 18:47:34 -0500

    Npm Scripts

--- server/bin/www.js
+++ server/bin/www.js
@@ -13,7 +13,6 @@ const debug = debugLib('myapp:server');
 /**
  * Get port from environment and store in Express.
  */
-
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
