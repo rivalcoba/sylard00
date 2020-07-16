2dc4f34ab3bed45a4255ee28a72e202614a7e0ec
rivalcoba <ivan.rivalcoba@gmail.com>
2020-07-16 18:38:04 -0500

    Webpack Installed 2

--- server/public/index.js
+++ server/public/index.js
@@ -94,7 +94,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_main_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/main.css */ \"./client/styles/main.css\");\n/* harmony import */ var _styles_main_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_styles_main_css__WEBPACK_IMPORTED_MODULE_0__);\n// Importando los estilos\n // Script Test\n\nvar root = document.createElement(\"div\");\nroot.innerHTML = \"<p>Hello Webpack.</p>\";\ndocument.body.appendChild(root);\nalert(\"Hola\");\n\nwindow.foo = function () {\n  alert(\"hola\");\n};\n\n//# sourceURL=webpack:///./client/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_main_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/main.css */ \"./client/styles/main.css\");\n/* harmony import */ var _styles_main_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_styles_main_css__WEBPACK_IMPORTED_MODULE_0__);\n// Importando los estilos\n // Script Test\n\nvar root = document.createElement(\"div\");\nroot.innerHTML = \"<p>Hello Webpack.</p>\";\ndocument.body.appendChild(root);\n\nwindow.foo = function () {\n  alert(\"hola\");\n};\n\n//# sourceURL=webpack:///./client/index.js?");
 
 /***/ }),
 
