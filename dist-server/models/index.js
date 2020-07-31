1abdb0b75709cf49ba3dfeaa72dc235d829124dd
rivalcoba <ivan.rivalcoba@gmail.com>
2020-07-31 12:37:37 -0500

    Produccion Confirmacion de cuenta

--- dist-server/models/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";var _User=_interopRequireDefault(require("./User"));Object.defineProperty(exports,"__esModule",{value:!0}),exports["default"]=void 0;function _interopRequireDefault(a){return a&&a.__esModule?a:{default:a}}// Importing app models
-// Exporting Model
-var _default={UserModel:_User["default"]};exports["default"]=_default;
