071bf1be579ecda83669e6426cdf0f2a17c52e20
rivalcoba <ivan.rivalcoba@gmail.com>
2020-08-07 14:08:14 -0500

    Prod Ruta /collections protegida

---
+++ dist-server/routes/collections.js
@@ -0,0 +1,5 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports["default"]=void 0;var _express=require("express"),_ensureAuth=_interopRequireDefault(require("../helpers/ensureAuth")),_ensureColabUser=_interopRequireDefault(require("../validators/ensureColabUser")),_collections=_interopRequireDefault(require("../controllers/collections"));function _interopRequireDefault(a){return a&&a.__esModule?a:{default:a}}// Imporntando el Enrutador
+// Creating an instance from the express router
+var router=new _express.Router;// Authorization Check Middleware
+router.get("/",_ensureAuth["default"],_ensureColabUser["default"],_collections["default"].index);// Se exportan rutas
+var _default=router;exports["default"]=_default;
--- dist-server/routes/routes.js
+++ dist-server/routes/routes.js
@@ -1,3 +1,3 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports["default"]=_default;var _index=_interopRequireDefault(require("./index")),_auth=_interopRequireDefault(require("./auth"));function _interopRequireDefault(a){return a&&a.__esModule?a:{default:a}}// Importing index router
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports["default"]=_default;var _index=_interopRequireDefault(require("./index")),_auth=_interopRequireDefault(require("./auth")),_collections=_interopRequireDefault(require("./collections"));function _interopRequireDefault(a){return a&&a.__esModule?a:{default:a}}// Importing index router
 function _default(a){// Other Routes
-return a.use("/",_index["default"]),a.use("/auth",_auth["default"]),a}
+return a.use("/",_index["default"]),a.use("/auth",_auth["default"]),a.use("/collections",_collections["default"]),a}
