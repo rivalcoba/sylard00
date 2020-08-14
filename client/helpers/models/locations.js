d4a817525b10dd70495da5bf068ff1dfb8cc2623
rivalcoba <ivan.rivalcoba@gmail.com>
2020-08-13 21:20:39 -0500

    cli help models

---
+++ client/helpers/models/locations.js
@@ -0,0 +1,11 @@
+import '@babel/polyfill'
+import client from '@client/utils/axios'
+
+const getByNomLoc = async function(nom_loc){
+    const response = await client.get(`/locations/index/nomloc/${nom_loc}`)
+    return response.data
+}
+
+export default{
+    getByNomLoc
+}
