6b7c9ab46e148c7bab07f82f3f3c0397f57d0b40
rivalcoba <ivan.rivalcoba@gmail.com>
2020-08-14 19:36:37 -0500

    Handle axios error

--- client/helpers/models/locations.js
+++ client/helpers/models/locations.js
@@ -2,7 +2,7 @@ import client from '@client/utils/axios'
 
 const getByNomLoc = async function(nom_loc){
     const response = await client.get(`/locations/index/nomloc/${nom_loc}`)
-    if(response.status = 200){
+    if(response.status == 200){
         return response.data
     }
     return null
