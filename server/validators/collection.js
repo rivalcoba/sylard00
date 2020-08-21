a6ca74169e94b9bc73b62a30550152048c005699
rivalcoba <ivan.rivalcoba@gmail.com>
2020-08-21 08:32:06 -0500

    validacion de collections funcionando

--- server/validators/collection.js
+++ server/validators/collection.js
@@ -7,50 +7,30 @@ import Location from '@models/Location'
 import User from '@models/User'
 
 // Validation Schema
-const getValidtaionSchema = function (supportedLanguages, supportedLocalities) {
-  const CollectionValidationSchema = Yup.object().shape({
-    name: Yup.string().required('Se requiere ingresar un nombre'),
-    description: Yup.string().required('Se requiere ingresar una descripcion'),
-    languages: Yup.array().of(Yup.string().oneOf(supportedLanguages,'No se ha proporcionado un lenguaje valido'), 'Se requiere un Arreglo de Strings'),
-    localities: Yup.array().of(Yup.string().oneOf(supportedLocalities,'No se han ingresado localidades validas'),'Se requiere un Arreglo de Strings'),
-  })
-  return CollectionValidationSchema
-}
+const CollectionValidationSchema = Yup.object().shape({
+  name: Yup.string().required('Se requiere ingresar un nombre'),
+  description: Yup.string().required('Se requiere ingresar una descripcion'),
+  languages: Yup.array().of(Yup.string(),'Se requiere un Arreglo de Strings'),
+  localities: Yup.array().of(Yup.string(),'Se requiere un Arreglo de Strings'),
+  license: Yup.string().oneOf(["CC-BY-NC-SA","CC BY-SA"],"Debe proporcionarse un tipo de licencia valida: CC-BY-NC-SA o CC BY-SA")
+})
 
-// name
-// description
-// languages
-// localities
-// license
 export default async (req, res, next) => {
     // vvvvv TEST Usuario se borrara
     let userdoc = await User.findById('5f31d909d49e576715d3f881','_id').exec()
     let user = userdoc._id
-    // ^^^^^ TEST Usuario se borrara
-
-    // Get Supported Languages
-    let languageDocuments = await Language.find({},'_id').limit(10).exec()
-    let supportedLanguages = languageDocuments.map((langDoc)=>{
-        return langDoc.id
-    })
-
-    // Get Supported localities
-    let localitiesDocuments = await Location.find({},'_id').limit(10).exec()
-    let supportedLocalities = localitiesDocuments.map((locDoc)=>{
-        return locDoc.id
-    })
-
-    // Get Validation Schema
-    const CollectionValidationSchema = getValidtaionSchema(supportedLanguages, supportedLocalities)
+    // ^^^^^ TEST Usuario se borrara  
 
     // Get Params from req
     let { languages, localities} = req.body
     const { name, description, license } = req.body
 
+    try {
     // Parse languages
     languages = languages.split('|')
     // parse localities
     localities = localities.split('|')
+
     // Form collection document
     let collection = {
         name,
@@ -61,27 +41,45 @@ export default async (req, res, next) => {
         user
     }
 
-    // Validate Collection
-    try {
-        collection = await CollectionValidationSchema.validate(collection,{abortEarly:false})
-        // Get collections
-        let languagesArr = await Language.find({"_id":{
+    // Se validan datos del formulario    
+    await CollectionValidationSchema.validate(collection,{abortEarly:false})
+    
+    // Se Buscan los lenguajes
+    // Get collections
+        let languagesDocs = await Language.find({"_id":{
             $in : languages
-        }},'gid family_id parent_id name level latitude longitude iso639P3code child_family_count child_language_count child_dialect_count country_ids').exec()
-        
-        collection.languages = languagesArr.map((doc)=>{
+        }},'gid family_id parent_id name level latitude longitude iso639P3code child_family_count child_language_count child_dialect_count country_ids').exec()        
+
+        if(languagesDocs.length == 0){
+            throw new Yup.ValidationError(
+                `Lenguajes incorrectos`,
+                req.body.languages,
+                'No se proporcionaron lenguajes correctos'
+              )
+        }
+        // Se puede refactorar y hacer desde el modelo
+        collection.languages = languagesDocs.map((doc)=>{
             let obj = doc.toJSON()
             delete obj._id
             return obj
         })
-
-        let localitiesArr = await Location.find({
+    
+        let localitiesDocs = await Location.find({
             "_id": {
                 $in : localities
             }
         },'Cve_Ent Nom_Ent Nom_Abr Cve_Mun Nom_Mun Cve_Loc Nom_Loc Lat_Decimal Lon_Decimal Altitud').exec()
 
-        collection.localities = localitiesArr.map((doc)=>{
+        if(localitiesDocs.length == 0){
+            throw new Yup.ValidationError(
+                `Localidades Incorrectas`,
+                req.body.localities,
+                'No se proporcionaron localidades correctas'
+              )
+        }
+        
+        // Se puede refactorar y hacer desde el modelo
+        collection.localities = localitiesDocs.map((doc)=>{
             let obj = doc.toJSON()
             delete obj._id
             return obj
