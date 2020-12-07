5b6d23bdbc6ca395365b94fbf6393253a17f2a2f
Beowulfdgo <antonioflores30@hotmail.com>
2020-12-07 17:26:05 -0600

    se añadio el componente EditAudioannotations

---
+++ client/templates/components/EditAudioannotations.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Hola</div>
+</template>
+
+<script>
+module.exports= {
+  name: "EditAudioannotations",
+  data() {
+    return {
+        who:"world"
+    };
+  },
+};
+</script>
+
+<style></style>
