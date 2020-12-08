a2d327f0188e159ebe3e16efa4d875ce4a62b734
Beowulfdgo <antonioflores30@hotmail.com>
2020-12-08 11:00:22 -0600

    se obtuvo la ruta de created vue

--- client/templates/components/EditAudioannotations.vue
+++ client/templates/components/EditAudioannotations.vue
@@ -1,15 +1,49 @@
 <template>
-  <div>Hola</div>
-</template>
+  <div id="EditAudioannotations"  >Hola {{who}} 
 
+{{ruta}}
+  </div> 
+</template>
 <script>
-module.exports= {
-  name: "EditAudioannotations",
+export default {
+  name: 'EditAudioannotations',
+  //  props:{idParametro:{
+  //     type: String,
+  //     default: '0',
+  // 	}},
+  props:['idParametro'],
   data() {
     return {
-        who:"world"
+        who:"world",
+        json: {},
+        ruta:"otro valor"
     };
   },
+  methods: {
+
+    },
+  //   props:['idParametro'],
+  computed: {
+    // imprimir(){
+    //   console.log("Ruta")
+    //   // const path = require('path');
+    //   // console.log(path.basename())
+    //   // console.log(this.idParametro)
+    //   // return this.idParametro
+    //  console.log(this.$route.query.test)
+
+    // }
+    
+  },  created() {
+    var currentUrl = window.location.pathname;
+        this.ruta=currentUrl  
+        //console.log(currentUrl);
+        var ultimoSlash = this.ruta.lastIndexOf("/"); 
+        this.ruta=this.ruta.substring(ultimoSlash+1)
+  },
+    beforeMount() {
+    },
+  
 };
 </script>
 
