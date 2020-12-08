eff127f6899a2652f7631ba9711911f70fe77f60
Alberto-Escamilla <l161130082@gamadero.tecnm.mx>
2020-12-08 14:42:23 -0600

    Probando estilos en modal SweetAlert2

--- client/helpers/modals.js
+++ client/helpers/modals.js
@@ -1,10 +1,11 @@
 import Swal from 'sweetalert2'
 
-const show= (title = "", content="")=>{
+const show = (title = "", content = "") => {
     Swal.fire({
-			title: `<strong>${title}</strong>`,
-      icon: 'info',
-      html: content,
+        title: `<span style="color:#3A96A2"><strong>${title}</strong></span>`,
+        html: content,
+        showCloseButton: true,
+        showConfirmButton: false,
     })
 }
 
