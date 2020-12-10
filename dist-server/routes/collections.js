12363995e6cda99995b73da2678a069fcda75e04
rivalcoba <ivan.rivalcoba@gmail.com>
2020-12-09 18:30:16 -0600

    PROD: Crud de Usuarios V0

--- dist-server/routes/collections.js
+++ dist-server/routes/collections.js
@@ -3,5 +3,7 @@
 const router=new _express.Router;// Authorization Check Middleware
 router.get("/",_ensureAuth.default,_ensureColabUser.default,_collections.default.index),router.get("/create",_ensureAuth.default,_ensureColabUser.default,_collections.default.createCollection),router.post("/add",_ensureAuth.default,_ensureColabUser.default,_collection.default,_collections.default.addCollection),router.get("/edit/:collection_id",_ensureAuth.default,_ensureColabUser.default,_collections.default.editCollectionForm),router.put("/edit/:collection_id",_ensureAuth.default,_ensureColabUser.default,_collectionsEdition.default,_collections.default.editCollection),router.delete("/delete/:collection_id",_ensureAuth.default,_ensureColabUser.default,_collections.default.deleteCollection),router.get("/api/read/:collection_id",/*ensureAuthenticated,
   ensureColabUser,*/ // TODO: Uncomment to protect route
-_collections.default.api_getCollectionById);// Se exportan rutas
+_collections.default.api_getCollectionById),router.get("/api/index/:userId",/*ensureAuthenticated,
+  ensureColabUser,*/ // TODO: Uncomment to protect route
+_collections.default.api_getCollectionByUser);// Se exportan rutas
 var _default=router;exports.default=_default;
