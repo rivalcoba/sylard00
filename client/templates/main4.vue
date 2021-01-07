d1eabf453b0eb35a0ba029e2311f67fcd196b57c
Alberto-Escamilla <l161130082@gamadero.tecnm.mx>
2021-01-06 20:45:39 -0600

    new main 4

---
+++ client/templates/main4.vue
@@ -0,0 +1,16 @@
+<template>
+<div id="app4">
+    <Dashboard></Dashboard>
+</div>
+</template>
+
+<script>
+import Dashboard from "./components/Dashboard.vue";
+
+export default {
+    name: "App4",
+    components: {
+        Dashboard,
+    }
+};
+</script>
