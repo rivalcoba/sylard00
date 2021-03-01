2d54f3836e177e77d598de1e4b8b8714a7f6e7f5
Alberto-Escamilla <l161130082@gamadero.tecnm.mx>
2021-03-01 13:32:43 -0600

    Se habilitan permisos para usuario invitado solo lectura

--- server/routes/audioannotations.js
+++ server/routes/audioannotations.js
@@ -4,7 +4,7 @@ import { Router } from 'express'
 import path from 'path'
 // Creating an instance from the express router
 const router = new Router()
-// Authorization Check Middleware
+    // Authorization Check Middleware
 import ensureAuthenticated from '@helpers/ensureAuth'
 import ensureColabUser from '@validators/ensureColabUser'
 
@@ -15,134 +15,134 @@ import multer from 'multer'
 var cors = require('cors')
 
 router.get(
-  '/',
-  ensureAuthenticated,
-  ensureColabUser,
-  audioannotationsController.index
+    '/',
+    ensureAuthenticated,
+    ensureColabUser,
+    audioannotationsController.index
 )
 
 router.get(
-  '/index/:audioannotationId',
-  ensureAuthenticated,
-  ensureColabUser,
-  audioannotationsController.indexById
+    '/index/:audioannotationId',
+    ensureAuthenticated,
+    //ensureColabUser,
+    audioannotationsController.indexById
 )
 
 router.post(
-  '/add',
-  ensureAuthenticated,
-  ensureColabUser,
-  audioannotationsController.addAudioannotation
+    '/add',
+    ensureAuthenticated,
+    ensureColabUser,
+    audioannotationsController.addAudioannotation
 )
 
 var storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    // resolving temp path
-    let eafPath = path.join(__dirname,'..','public','eaf')
-    console.log(`>> eafPath: ${eafPath}`);
-    cb(null, eafPath)
-  },
-  filename: function(req, file, cb) {
-    let fileName = file.originalname + '-' + Date.now() + '.eaf'
-    req.fname = fileName
-    cb(null, fileName)
-    // save this on the app object as a configuration
-  },
+    destination: function(req, file, cb) {
+        // resolving temp path
+        let eafPath = path.join(__dirname, '..', 'public', 'eaf')
+        console.log(`>> eafPath: ${eafPath}`);
+        cb(null, eafPath)
+    },
+    filename: function(req, file, cb) {
+        let fileName = file.originalname + '-' + Date.now() + '.eaf'
+        req.fname = fileName
+        cb(null, fileName)
+            // save this on the app object as a configuration
+    },
 })
 
 var upload = multer({ storage: storage })
 router.post(
-  '/uploadfile',
-  upload.single('myFile'),
-  cors(),
-  ensureAuthenticated,
-  ensureColabUser,
-  audioannotationsController.uploadfileAudioannotation
+    '/uploadfile',
+    upload.single('myFile'),
+    cors(),
+    ensureAuthenticated,
+    ensureColabUser,
+    audioannotationsController.uploadfileAudioannotation
 )
 
 // Show Form to create a audioannotation
 router.get(
-  '/create',
-  ensureAuthenticated,
-  cors(),
-  ensureColabUser,
-  audioannotationsController.createAudioannotation
+    '/create',
+    ensureAuthenticated,
+    cors(),
+    ensureColabUser,
+    audioannotationsController.createAudioannotation
 )
 
 router.get(
-  '/edit/:audioannotation_id',
-  ensureAuthenticated,
-  ensureColabUser,
-  audioannotationsController.editAudioannotation
+    '/edit/:audioannotation_id',
+    ensureAuthenticated,
+    ensureColabUser,
+    audioannotationsController.editAudioannotation
 )
 
 // Update a Collection
 router.put(
-  '/edit/:audioannotation_id',
-  ensureAuthenticated,
-  ensureColabUser,
-  audioannotationsController.editAudioannotation
-)
-// Delete a Collection
+        '/edit/:audioannotation_id',
+        ensureAuthenticated,
+        ensureColabUser,
+        audioannotationsController.editAudioannotation
+    )
+    // Delete a Collection
 router.delete(
-  '/delete/:audioannotation_id',
-  //ensureAuthenticated,
-  //ensureColabUser,
-  audioannotationsController.deleteAudioannotaion
+    '/delete/:audioannotation_id',
+    //ensureAuthenticated,
+    //ensureColabUser,
+    audioannotationsController.deleteAudioannotaion
 )
 
 //router.get(
 //  '/vuetest/:audioannotation_id',
 router.get(
-  '/vuetest/:audioannotation_id',
-  
-  ensureAuthenticated,
-  ensureColabUser,
-  audioannotationsController.vuetestAudioannotaion
+    '/vuetest/:audioannotation_id',
+
+    ensureAuthenticated,
+    //ensureColabUser,
+    audioannotationsController.vuetestAudioannotaion
 )
 
 router.get(
-  '/filter/:page',
-   ensureAuthenticated,
-   ensureColabUser,
-  audioannotationsController.filtrarAudioannotation
+    '/filter/:page',
+    ensureAuthenticated,
+    //ensureColabUser,
+    audioannotationsController.filtrarAudioannotation
 )
 
 
 router.get(
-  '/color',
-  ensureAuthenticated,
-  ensureColabUser,
-  audioannotationsController.color)
-  
-  // Configurar cabeceras y cors https://filesamples.com/formats/mp3 //no funciona marca error en cors
-  //router.get('/', function(req, res) {
-    //    res.setHeader('Access-Control-Allow-Origin', '*');
-    //    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    //    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-    //    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-    
-    //    res.send('cors problem fixed:)');
-    //});
-    
-    //   ___  ______ _____ 
-    //  / _ \ | ___ \_   _|
-    // / /_\ \| |_/ / | |  
-    // |  _  ||  __/  | |  
-    // | | | || |    _| |_ 
-    // \_| |_/\_|    \___/ 
-    
-    router.post('/api/update/:audioannotationId',
+    '/color',
+    ensureAuthenticated,
+    //ensureColabUser,
+    audioannotationsController.color)
+
+// Configurar cabeceras y cors https://filesamples.com/formats/mp3 //no funciona marca error en cors
+//router.get('/', function(req, res) {
+//    res.setHeader('Access-Control-Allow-Origin', '*');
+//    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+//    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+//    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+//    res.send('cors problem fixed:)');
+//});
+
+//   ___  ______ _____ 
+//  / _ \ | ___ \_   _|
+// / /_\ \| |_/ / | |  
+// |  _  ||  __/  | |  
+// | | | || |    _| |_ 
+// \_| |_/\_|    \___/ 
+
+router.post('/api/update/:audioannotationId',
     /*ensureAuthenticated,
     ensureColabUser,*/ // TODO: Uncomment to protect route
     audioannotationsController.api_updateAudioAnnot
+)
+
+router.get(
+        '/api/index/:id/:page',
+        //ensureAuthenticated,
+        //ensureColabUser,
+        audioannotationsController.api_indexAudioannotationsByCollection
     )
-    
-    router.get(
-      '/api/index/:id/:page',
-      //ensureAuthenticated,
-      //ensureColabUser,
-      audioannotationsController.api_indexAudioannotationsByCollection
-    )
-// Se exportan rutas
+    // Se exportan rutas
 export default router
