40cbfbc7a90912464cffad60bc1d5b0de5ca3f08
lokimenethill <xdjtrancemetal@hotmail.com>
2021-03-09 04:32:44 -0600

    Mejora-buscador-datatables

--- client/templates/components/CompUsuarios.vue
+++ client/templates/components/CompUsuarios.vue
@@ -1,13 +1,16 @@
 <template>
+<section class="container">
+  <div class="col-sm-12 contenedor-90vh" id="contenedor_general_mis_colecciones">
+      <div class="contenedor_tabla_mis_colecciones">
  <div class="content">
        <el-input
           v-model="search"
           size="mini"
-          placeholder="Type to search"
+          placeholder="Escribe para buscar"
           class="contenedor_etiquetas_barras_busqueda"
           />
       <el-table
-    :data="result.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+    :data="result.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()) || data.email.toLowerCase().includes(search.toLowerCase()) || data.role.toLowerCase().includes(search.toLowerCase()) || data.about.toLowerCase().includes(search.toLowerCase()))"
     :default-sort = "{prop: 'date', order: 'descending'}"
     style="width: 100%"
     class="tabla_todos_los_usuarios"
@@ -50,6 +53,9 @@
 
   </el-table>
   </div>
+      </div>
+  </div>
+  </section>
  
 </template>
 <script>
