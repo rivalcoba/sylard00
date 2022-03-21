c563c8caf81c6fe739c3e40d64c031b7586c2f75
yoali <admon@mail.com>
2022-03-21 13:05:00 -0600

    Se soluciona PORT

--- server/bin/www.js
+++ server/bin/www.js
@@ -47,18 +47,18 @@ function onError(error) {
     throw error;
   }
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  var bind = typeof PORT === 'string'
+    ? `Pipe ${PORT}`
+    : `Port ${PORT}`;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+      console.error(`requires elevated privileges ${bind}`);
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+      console.error(`${bind} is already in use`);    
       process.exit(1);
       break;
     default:
